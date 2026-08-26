3bc2599dba0053d9f26f4c4a0c1660c7d5cf249f
IgorAlexandre <igoralexandrevieira652@gmail.com>
2026-08-26 12:38:40 -0400

    Add EditorConfig and update ESLint configuration with TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,44 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  "env", {
+      "browser": true,
+      "es2021": true,
+      "node": true
+  },
+  "extends", [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:prettier/recommended"
+  ],
+  "parser", "@typescript-eslint/parser",
+  "parserOptions", {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 12,
+      "sourceType": "module"
+  },
+  "plugins", [
+      "react",
+      "@typescript-eslint"
+  ],
+  "settings", {
+      "react": {
+          "version": "detect"
+      }
+  },
+  "rules", {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
 ]);
-
-export default eslintConfig;
